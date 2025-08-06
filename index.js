ba02c858585c98f482cf7509739be063cac15975
Alex Peter Onoja <alexpeteronoja@gmail.com>
2025-08-06 15:42:51 +0100

    edited the backend api

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
-const __filename = fileURLToPath(
-    import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
@@ -21,11 +20,12 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-    cors({
-        origin: process.env.NODE_ENV === "production" ?
-            "https://image-uploader-omega-three.vercel.app/" :
-            "http://localhost:5173",
-    })
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? "https://image-uploader-omega-three.vercel.app"
+        : "http://localhost:5173",
+  })
 );
 app.use(helmet());
 app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
@@ -33,74 +33,74 @@ app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
 // app.use(express.static("public"));
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "./public/image");
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname.toLowerCase());
-    },
+  destination: function (req, file, cb) {
+    cb(null, "./public/image");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname.toLowerCase());
+  },
 });
 
 const upload = multer({
-    storage: storage,
-    limits: { fileSize: 1024 * 1024 * 2 },
-    fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png/;
-        const extName = fileTypes.test(
-            path.extname(file.originalname).toLowerCase()
-        );
-        const mimeType = fileTypes.test(file.mimetype);
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 2 },
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png/;
+    const extName = fileTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimeType = fileTypes.test(file.mimetype);
 
-        const existingFile = path.join(
-            __dirname,
-            "public",
-            "image",
-            file.originalname.toLowerCase()
-        );
+    const existingFile = path.join(
+      __dirname,
+      "public",
+      "image",
+      file.originalname.toLowerCase()
+    );
 
-        if (extName && mimeType) {
-            return cb(null, true);
-        } else {
-            cb(
-                new Error(
-                    "Error Only JPG, JPEG, and PNG file Type allowed or File Exist"
-                )
-            );
-        }
-    },
+    if (extName && mimeType) {
+      return cb(null, true);
+    } else {
+      cb(
+        new Error(
+          "Error Only JPG, JPEG, and PNG file Type allowed or File Exist"
+        )
+      );
+    }
+  },
 }).single("file");
 
 app.post("/api/upload", (req, res) => {
-    upload(req, res, function(err) {
-        if (err) {
-            return res.status(400).json({ error: err || "upload failed" });
-        }
+  upload(req, res, function (err) {
+    if (err) {
+      return res.status(400).json({ error: err || "upload failed" });
+    }
 
-        if (!req.file) {
-            return res.status(400).json({ error: "No files Uploaded" });
-        }
+    if (!req.file) {
+      return res.status(400).json({ error: "No files Uploaded" });
+    }
 
-        res.status(200).json({ message: "Succesfully Uploaded", file: req.file });
-    });
+    res.status(200).json({ message: "Succesfully Uploaded", file: req.file });
+  });
 });
 
 app.get("/api/download/:filename", (req, res) => {
-    const filename = req.params.filename;
-    const sentFile = path.join(__dirname, "public", "image", filename);
+  const filename = req.params.filename;
+  const sentFile = path.join(__dirname, "public", "image", filename);
 
-    // Check if directory exist
+  // Check if directory exist
 
-    access(sentFile, constants.F_OK, (err) => {
-        if (err) {
-            console.error(`${sentFile} does not exist`);
-            return res.status(404).send("file does not exist");
-        }
+  access(sentFile, constants.F_OK, (err) => {
+    if (err) {
+      console.error(`${sentFile} does not exist`);
+      return res.status(404).send("file does not exist");
+    }
 
-        res.sendFile(sentFile);
-        console.log(`${sentFile} file sent`);
-    });
+    res.sendFile(sentFile);
+    console.log(`${sentFile} file sent`);
+  });
 });
 
 app.listen(process.env.Port || 3000, () =>
-    console.log("Server Started at Port 3000")
+  console.log("Server Started at Port 3000")
 );
